e52e814fa972fd86a0fcff0db8373479c693e94f
Anwesh43 <anweshthecool0@gmail.com>
2023-07-01 19:31:37 +0530

    adding changes to import TemplateProcessor

--- chat_prompt_template.py
+++ chat_prompt_template.py
@@ -23,38 +23,38 @@ class TemplateProcessor:
         return response 
 
         
+if __name__ == "__main__":
+    template_string = "Translate the text that is delimited by triple backticks into a style that is {style}. text: ```{text}```"
+    # chat = ChatOpenAI()
+    # print(chat)
+    pt = TemplateProcessor(template_string)
+    print(pt.get_variables())
 
-template_string = "Translate the text that is delimited by triple backticks into a style that is {style}. text: ```{text}```"
-# chat = ChatOpenAI()
-# print(chat)
-pt = TemplateProcessor(template_string)
-print(pt.get_variables())
-
-customer_style = "American English in a calm and respectful tone"
-customer_email = "Arrr, I be fuming that me blender lid flew off and splattered me kitchen walls with smoothie! And to make matters worse, the warranty don't cover the cost of cleaning up me kitchen. I need yer help right now, matey!"
+    customer_style = "American English in a calm and respectful tone"
+    customer_email = "Arrr, I be fuming that me blender lid flew off and splattered me kitchen walls with smoothie! And to make matters worse, the warranty don't cover the cost of cleaning up me kitchen. I need yer help right now, matey!"
 
 
-print("Getting customer response")
+    print("Getting customer response")
 
-response = pt.format_and_chat(style = customer_style, text = customer_email)
+    response = pt.format_and_chat(style = customer_style, text = customer_email)
 
-print(response.content)
+    print(response.content)
 
-service_reply = """Hey there customer, \
-the warranty does not cover \
-cleaning expenses for your kitchen \
-because it's your fault that \
-you misused your blender \
-by forgetting to put the lid on before \
-starting the blender. \
-Tough luck! See ya!
-"""
+    service_reply = """Hey there customer, \
+    the warranty does not cover \
+    cleaning expenses for your kitchen \
+    because it's your fault that \
+    you misused your blender \
+    by forgetting to put the lid on before \
+    starting the blender. \
+    Tough luck! See ya!
+    """
 
-service_style_pirate = """\
-a polite tone \
-that speaks in English Pirate\
-"""
+    service_style_pirate = """\
+    a polite tone \
+    that speaks in English Pirate\
+    """
 
-print("Getting Pirate response")
-service_response = pt.format_and_chat(style = service_style_pirate, text = service_reply)
-print(service_response.content)
+    print("Getting Pirate response")
+    service_response = pt.format_and_chat(style = service_style_pirate, text = service_reply)
+    print(service_response.content)
