6a9d728fc972c7fba18797e9a096d684feac6746
Anwesh43 <anweshthecool0@gmail.com>
2023-07-03 18:26:11 +0530

    adding SequentialChain

--- chain_processor.py
+++ chain_processor.py
@@ -15,13 +15,13 @@ class ChainProcessor:
 
 
 class ChainProcessorSequence:
-    def __init__(self, chains = [], type = 1, verbose = True):
+    def __init__(self, chains = [], type = 1, verbose = True, input_variables = [], output_variables = []):
         print(chains)
         if type == 1:
             self.chain = SimpleSequentialChain(chains = chains, verbose = verbose)
         
         if type == 2:
-            self.chain = SequentialChain(chains = chains, verbose = verbose)
+            self.chain = SequentialChain(chains = chains, verbose = verbose, input_variables = input_variables, output_variables = output_variables)
     
     def run(self, text):
         return self.chain.run(text)
@@ -30,8 +30,14 @@ if __name__ == "__main__":
     cp1 = ChainProcessor(template = "What can be a good name for a company producing the product : {product}?", output_key = "company_name")
     #print(cp1.run("Clothes"))
 
-    cp2 = ChainProcessor(template = "Write a description about the company in 20 words : {company_name}", output_key = "company_description")
+    cp2 = ChainProcessor(template = "Write a description about the company in 100 words : {company_name}", output_key = "company_description")
 
     sc1 = ChainProcessorSequence(chains = [cp1.chain, cp2.chain], verbose = False)
-
     print(sc1.run("Software"))
+
+    cp3 = ChainProcessor(template = "How was the description {company_description}, good or bad?", output_key = "desc_type")
+
+    cp4 = ChainProcessor(template = "Overall json object for {company_description}, {desc_type} and {company_name}", output_key = "json_obj")
+
+    # sc2 = ChainProcessorSequence(type = 2, chains = [cp1.chain, cp2.chain, cp3.chain, cp4.chain], input_variables = ["product"], output_variables = [ "company_name", "company_description", "desc_type", "json_obj"])
+    # print(sc2.chain("Paper"))
